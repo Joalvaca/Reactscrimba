32090a308985a4d1530c101333c73bcbfbbb52fb
joel alva <panthersjoe@hotmail.com>
2020-05-04 01:47:24 -0400

    test

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from "react"
-import "./App.css"
-import Joke from "./components/Joke"
+import React from "react";
+import "./App.css";
+import Joke from "./components/Joke";
 
 function App() {
   return (
@@ -8,9 +8,9 @@ function App() {
       <Joke question="kddd" joke="j" />
       <Joke question="atest" joke="b" />
       <Joke question="ctest" joke="c" />
-      <Joke question="k" joke="j" />
+      <Joke question="ktest" joke="j" />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
