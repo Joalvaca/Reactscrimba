e2a0fa2caeb45ff3a110f3fd468149816b1e0a7c
joel alva <panthersjoe@hotmail.com>
2020-05-05 20:22:30 -0400

    updatess

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 import React from "react";
-import "./App.css";
-// import Joke from "./components/Joke";
-// import jokesData from "./components/jokesData";
-import Data from "./components/Products/Data";
-import Products from "./components/Products/Products";
 
-// function App() {
-//   let jokeComponents = jokesData.map((joke) => {
-//     console.log(joke);
-//     return (
-//       <Joke question={joke.question} punchLine={joke.punchLine} key={joke.id} />
-//     );
-//   });
-
-//   return <div>{jokeComponents}</div>;
-// }
-
-function App() {
-  let products = Data.map((item) => {
-    return <Products key={item.id} products={item} />;
-  });
-
-  return <div>{products}</div>;
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Code</h1>
+      </div>
+    );
+  }
 }
 export default App;
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header username="Joel" />
+      </div>
+    );
+  }
+}
+
+export default App;
+
+class Header extends React.Component {
+  render() {
+    return <div>Hello, {this.props.username}</div>;
+  }
+}
+
+export default Header;
