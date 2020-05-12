adca25ed0b023ab1436b87146960ebb3cf01369c
joel alva <panthersjoe@hotmail.com>
2020-05-12 02:49:46 -0400

    updates

--- src/App.js
+++ src/App.js
@@ -4,37 +4,42 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      firstName: "",
-      lastName: "",
+      count: 1,
+      total: "",
     };
   }
+
+  handlechange = () => {
+    this.setState((prevState) => {
+      return {
+        count: prevState.count,
+      };
+    });
+  };
 
   handleChange = (e) => {
     const { name, value } = e.target;
     this.setState({ [name]: value });
   };
+
+  add = (e) => {
+    let x = this.prevState.count + this.state.total;
+    this.setState({ result: x });
+  };
+
   render() {
     return (
       <div>
-        <h1>hi</h1>
+        <h1>{this.state.count}</h1>
+        <button onClick={this.handlechange}>Click</button>
+
         <form>
           <input
-            name="firstName"
-            value={this.state.firstName}
             type="text"
-            placeholder="first name"
+            name="total"
+            value={this.state.total}
             onChange={this.handleChange}
           ></input>
-          <input
-            name="lastName"
-            type="text"
-            value={this.state.lastName}
-            placeholder="last name"
-            onChange={this.handleChange}
-          ></input>
-          <h1>
-            {this.state.firstName} {this.state.lastName}
-          </h1>
         </form>
       </div>
     );
