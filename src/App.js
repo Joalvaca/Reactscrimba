08ce6413b09adaf77034905b57f7c2ec6d1292e6
joel alva <panthersjoe@hotmail.com>
2020-05-12 16:41:09 -0400

    test

--- src/App.js
+++ src/App.js
@@ -1,49 +1,8 @@
 import React, { Component } from "react";
+import FormState from "./components/ReactContainer/FormState";
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      count: 1,
-      total: "",
-    };
-  }
-
-  handlechange = () => {
-    this.setState((prevState) => {
-      return {
-        count: prevState.count,
-      };
-    });
-  };
-
-  handleChange = (e) => {
-    const { name, value } = e.target;
-    this.setState({ [name]: value });
-  };
-
-  add = (e) => {
-    let x = this.prevState.count + this.state.total;
-    this.setState({ result: x });
-  };
-
-  render() {
-    return (
-      <div>
-        <h1>{this.state.count}</h1>
-        <button onClick={this.handlechange}>Click</button>
-
-        <form>
-          <input
-            type="text"
-            name="total"
-            value={this.state.total}
-            onChange={this.handleChange}
-          ></input>
-        </form>
-      </div>
-    );
-  }
+function App() {
+  return <FormState />;
 }
 
 export default App;
