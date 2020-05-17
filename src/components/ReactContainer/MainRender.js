d32aa1d143ef990eb0222eeb4f9c0b02c2272462
joel alva <panthersjoe@hotmail.com>
2020-05-16 22:05:06 -0400

    test

---
+++ src/components/ReactContainer/MainRender.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import FormState from "./components/ReactContainer/FormState";
+
+function mainRender() {
+  return <FormState />;
+}
+
+export default mainRender;
